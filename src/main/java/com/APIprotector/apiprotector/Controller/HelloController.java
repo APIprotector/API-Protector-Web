9787a8cbc7fc9108e99dde4962087b2b64af5b99
Dawid Jabłoński <xederro@gmail.com>
2025-03-09 16:25:01 +0100

    Site Configuration

--- src/main/java/com/APIprotector/apiprotector/Controller/HelloController.java
+++ src/main/java/com/APIprotector/apiprotector/Controller/HelloController.java
@@ -4,14 +4,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 @RestController
 @RequestMapping("/api/hello")
 public class HelloController {
+
+    private static Random random = new Random();
+    private static ArrayList<String> helloWorlds = new ArrayList<>(
+            java.util.List.of(
+                    "Hello, World!", "Hola, Mundo!", "Bonjour, le Monde!", "Hallo, Welt!", "Ciao, Mondo!",
+                    "Привет, мир!", "你好，世界！", "こんにちは、世界！", "안녕하세요, 세계!", "Olá, Mundo!",
+                    "Merhaba, Dünya!", "नमस्ते, दुनिया!", "Salam, Dunia!", "Hej, Världen!", "Hallo, Wereld!",
+                    "Hei, Maailma!", "Halo, Dunia!", "Γειά σου, Κόσμε!", "שלום, עולם!", "مرحبا، العالم!"
+            )
+    );
+
     public HelloController() {
     }
 
     @GetMapping
     public String getHello() {
-        return "Hewwo, Wowd :3";
+        return helloWorlds.get(random.nextInt(helloWorlds.size()));
     }
 }
